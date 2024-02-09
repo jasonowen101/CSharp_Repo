2fafbae8d3e3fbb1ef53a968f8fed21064610544
Jason Owen <jasonowen101@gmail.com>
2024-02-09 11:02:30 -0600

    232_CSharp_Repo

---
+++ NewAppCSharp1/Bye.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NewAppCSharp1
+{
+    class Bye
+    {
+        public void Duces()
+        {
+            Console.WriteLine("Bye World!");
+        }
+    }
+}
---
+++ NewAppCSharp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NewAppCSharp1
+{
+    class Program
+    {
+        static void Howdy()
+            {
+                Console.WriteLine("Howdy World!");
+            }
+
+        static void Main(string[] args)
+        {
+            Bye yo = new Bye();
+            Howdy();
+            yo.Duces();
+        }
+    }
+}
